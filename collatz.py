f8bc21f7f2e5138eda479687090b55ed633b0cba
Morgan <morgan.batterham@gmail.com>
2020-05-21 20:49:21 +1000

    added exception handling to collatz.py

--- collatz.py
+++ collatz.py
@@ -1,12 +1,15 @@
 def collatz(number):
-    while number != 1:
-        if number % 2 == 0:
-            number = number // 2
-            print(number)
-        else:
-            number = 3 * number + 1
-            print(number)
-
-collatz(3)
-        
-    
+    try:
+        number = int(number)
+        while number != 1:
+            if number % 2 == 0:
+                number = number // 2
+                print(number)
+            else:
+                number = 3 * number + 1
+                print(number)
+    except ValueError:
+        print('Please enter a valid number')
+print('Please enter a number: ')
+input = input()
+collatz(input)
