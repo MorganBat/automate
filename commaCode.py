2e0d5b8907a91e35cda9789bad74650a5aca3bcc
Morgan <morgan.batterham@gmail.com>
2020-06-08 13:50:01 +1000

    updated commaCode.py

--- commaCode.py
+++ commaCode.py
@@ -1,6 +1,10 @@
 def commaCode(lst):
     ans = []
     i = 0
+    if len(lst) == 0:
+        return ''
+    elif len(lst) == 1:
+        return lst[0]
 
     while i < (len(lst) - 2) :
         ans.append(lst[i])
@@ -14,3 +18,7 @@ def commaCode(lst):
     return ''.join(ans)
 
 print(commaCode(['apples', 'banana','tofu', 'cats']))
+print(commaCode([]))
+print(commaCode(['apples']))
+print(commaCode(['apples', 'banana']))
+print(commaCode(['apples', 'banana', 'tofu']))
